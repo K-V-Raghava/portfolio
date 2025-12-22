111cb7968235e399c448ed41da765f62fa71484e
K-V-Raghava <venkata.k1324@gmail.com>
2025-12-23 02:14:36 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "K V Raghava Karthikeya",
   profile: '/profile.png',
-  designation: "AI ML & Computer Vision Engineer and Web & Mobile Developer",
+  designation: "AI ML & Computer Vision Engineer and  Web & Mobile Developer",
   description: "My name is Raghava. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving.I’m passionate about building intelligent systems that bridge the gap between data and real-world impact. My core expertise lies in AI and machine learning, and I enjoy leveraging Python and modern frameworks to develop data-driven applications. I also have experience in web technologies, which helps me integrate ML models seamlessly into web platforms. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'venkata.k1324@gmail.com',
   phone: '+91 9391453402',
