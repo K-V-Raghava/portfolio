932e4f577b6fb836aad0cd711c6d4f0cc640db13
K-V-Raghava <venkata.k1324@gmail.com>
2026-02-22 13:46:32 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "I am a professional and enthusiastic programmer. I am a quick learner with a self-learning attitude. I am passionate about problem-solving and love to work with latest technologies. I’m passionate about building intelligent systems that bridge the gap between data and real-world impact. My core expertise lies in AI ML concepts and I enjoy leveraging Python and modern frameworks to develop data-driven applications. I also have experience in web technologies, which helps me integrate ML models seamlessly into web platforms. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'venkata.k1324@gmail.com',
   address: 'Hyd , Telangana',
-  github: 'https://github.com/K-V-Raghava',
+  github: 'https://github.com/b23bb1023',
   linkedIn: 'https://www.linkedin.com/in/raghavakv/',
-  resume: "-------------"
+  resume: "https://drive.google.com/file/d/1qxRxSUCx8H9bWsX-EqCm2OV8_NIijEBM/view?usp=drive_link"
 }
