402c990f71aacecff140b8ca326c9d898ee3621c
K-V-Raghava <venkata.k1324@gmail.com>
2026-02-22 13:47:29 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'venkata.k1324@gmail.com',
     address: 'Hyd , Telangana ',
-    github: 'https://github.com/K-V-Raghava',
+    github: 'https://github.com/b23bb1023',
     linkedIn: 'www.linkedin.com/in/raghavakv',
 }
